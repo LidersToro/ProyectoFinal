1c21c1c01a3b2c82703cf1757d22cf1904a01ec7
sebas-9009 <36604958+sebas-9009@users.noreply.github.com>
2020-06-22 12:16:12 -0400

    cambios para iniciar como motoquero

--- controlador/controladorlogin.php
+++ controlador/controladorlogin.php
@@ -8,16 +8,20 @@ $_SESSION['clave'] = $clave;
 require_once __DIR__.'/../modelo/clienteModelo.php';
 require_once __DIR__.'/../modelo/EmpresaModelo.php';
 require_once __DIR__.'/../modelo/AdminModelo.php';
+require_once __DIR__.'/../modelo/MotoqueroModelo.php';
 $Obj = new cliente();
 $ObjEmp = new empresa();
 $ObjAdm = new admin();
+$ObjMot = new motoquero();
 if(isset($_POST['btn_login'])) 
 {                      
      $row = $Obj->obtenerCuenta($user,$clave);
      $rowEmp = $ObjEmp->obtenerCuenta($user,$clave);
      $rowAdm = $ObjAdm->obtenerCuenta($user,$clave);
+     $rowMot = $ObjMot->obtenerCuenta($user,$clave);
      $fila=$row->fetch_row();
      $filaEmp=$rowEmp->fetch_row();
+     $filaMot=$rowMot->fetch_row();
 
      $filaAdm=$rowAdm->fetch_row();
             if($fila[1]==$user && $fila[4]==$clave)
@@ -32,14 +36,22 @@ if(isset($_POST['btn_login']))
                 $_SESSION['id'] = $filaEmp[0];
                 }else
                 {
-                         if($filaAdm[1]==$user && $filaAdm[2]==$clave)
+                    if($filaMot[9]==$user && $filaMot[3]==$clave)
                         {
-                        echo " <script>window.location = '/proyectoFinal/vista/empresagestion.php';</script>";
+                         echo " <script>window.location = '/proyectoFinal/vista/pedidogestion.php';</script>";
                         }
                         else
                         {
-                            echo "<script>alert('NOMBRE DE USUARIO O CONTRASENA INCORRECTO!');</script>";
-                            echo " <script>window.location = '/proyectoFinal/vista/login.html';</script>"; 
+
+                             if($filaAdm[1]==$user && $filaAdm[2]==$clave)
+                                {
+                                    echo " <script>window.location = '/proyectoFinal/vista/empresagestion.php';</script>";
+                                }
+                             else
+                                {
+                                    echo "<script>alert('NOMBRE DE USUARIO O CONTRASENA INCORRECTO!');</script>";
+                                    echo " <script>window.location = '/proyectoFinal/vista/login.html';</script>"; 
+                                }
                         }
                  }
             }
@@ -47,7 +59,8 @@ if(isset($_POST['btn_login']))
 
      
 }
-
+$_SESSION['userEmpr']=$filaEmp[1];
+$_SESSION['userMot']=$filaMot[1];
 $_SESSION['user'] = $fila[2];
 
 
--- vista/cliente/company-grid.php
+++ vista/cliente/company-grid.php
@@ -240,7 +240,7 @@ $idEmpresa = $_GET['pid'];
                         </div>
                         <div class="col-lg-4 col-md-6 col-sm-6">
                             <div class="product__item">
-                                <div class="product__item__pic set-bg" data-setbg="img/product/product-2.jpg">
+                                <div class="product__item__pic set-bg">
                                 <ul class="product__item__pic__hover">
                                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
                                 </ul>
